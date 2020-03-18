7e553bbfa2b5ed2d53109e973a1b8b7bfac8a359
novakvova <novakvova@gmail.com>
2020-03-18 16:02:32 +0200

    Add login frame

--- app/src/main/java/com/example/begin/MainActivity.java
+++ app/src/main/java/com/example/begin/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.begin;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,12 +14,40 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new LoginFragment())
+                    .commit();
+        }
+
+//        new MaterialAlertDialogBuilder(MainActivity.this)
+//                .setTitle("Title")
+//                .setMessage("Your message goes here. Keep it short but clear.")
+//                .setPositiveButton("GOT IT", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//
+//                    }
+//                })
+//                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//
+//                    }
+//                })
+//                .show();
+
     }
 
     @Override
@@ -47,13 +76,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void onButtonClick(View view)
-    {
-        TextView vText=findViewById(R.id.tvInfo);
-        EditText txtInfo = findViewById(R.id.txtInfo);
-        String text = txtInfo.getText().toString();
-        vText.setText(text);
-        Toast.makeText(MainActivity.this,
-                text, Toast.LENGTH_LONG).show();
-    }
+//    public void onButtonClick(View view)
+//    {
+//        TextView vText=findViewById(R.id.tvInfo);
+//        EditText txtInfo = findViewById(R.id.txtInfo);
+//        String text = txtInfo.getText().toString();
+//        vText.setText(text);
+//        Toast.makeText(MainActivity.this,
+//                text, Toast.LENGTH_LONG).show();
+//    }
 }
