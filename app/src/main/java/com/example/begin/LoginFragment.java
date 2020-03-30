1e5556d4ac6a4ca033077ef3d799e8a844922bce
novakvova <novakvova@gmail.com>
2020-03-30 17:08:10 +0300

    Begin work jwt token

--- app/src/main/java/com/example/begin/LoginFragment.java
+++ app/src/main/java/com/example/begin/LoginFragment.java
@@ -2,28 +2,32 @@ package com.example.begin;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.text.Editable;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.begin.account.AccountService;
+import com.example.begin.account.LoginDTO;
+import com.example.begin.account.TokenDTO;
 import com.example.begin.productview.ProductGridFragment;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LoginFragment extends Fragment {
 
+    private static final String TAG = LoginFragment.class.getSimpleName();
     public LoginFragment() {
         // Required empty public constructor
     }
@@ -41,6 +45,7 @@ public class LoginFragment extends Fragment {
 
         final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
         final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
+        final TextInputEditText phoneEditText = view.findViewById(R.id.telephone_edit_text);
         MaterialButton nextButton = view.findViewById(R.id.next_button);
 
         // Set an error if the password is less than 8 characters.
@@ -51,7 +56,41 @@ public class LoginFragment extends Fragment {
                     passwordTextInput.setError("Пароль має бути мін 8 символів");
                 } else {
                     passwordTextInput.setError(null); // Clear the error
-                    ((NavigationHost) getActivity()).navigateTo(new ProductGridFragment(), false); // Navigate to the next Fragment
+                    String login = phoneEditText.getText().toString();
+                    String password = passwordEditText.getText().toString();
+
+                    LoginDTO loginDTO=new LoginDTO(login, password);
+                    AccountService.getInstance()
+                            .getJSONApi()
+                            .loginRequest(loginDTO)
+                            .enqueue(new Callback<TokenDTO>() {
+                                @Override
+                                public void onResponse(@NonNull Call<TokenDTO> call, @NonNull Response<TokenDTO> response) {
+                                    if(response.isSuccessful()) {
+                                        TokenDTO tokenDTO = response.body();
+                                        //((JwtServiceHolder) getActivity()).SaveJWTToken(tokenDTO.getToken()); // Navigate to the register Fragment
+                                        //((NavigationHost) getActivity()).navigateTo(new ProductGridFragment(), false); // Navigate to the products Fragment
+                                        Log.e(TAG,"*************GOOD Request***********"+ tokenDTO.getToken());
+                                    }
+                                    else {
+                                        Log.e(TAG,"_______________________"+response.errorBody().charStream());
+                                    }
+
+                                    //Log.d(TAG,tokenDTO.toString());
+                                    //CommonUtils.hideLoading();
+                                }
+
+                                @Override
+                                public void onFailure(@NonNull Call<TokenDTO> call, @NonNull Throwable t) {
+                                    //CommonUtils.hideLoading();
+                                    Log.e("ERROR","*************ERORR request***********");
+                                    t.printStackTrace();
+                                }
+                            });
+
+
+
+                    //((NavigationHost) getActivity()).navigateTo(new ProductGridFragment(), false); // Navigate to the next Fragment
                 }
             }
         });
