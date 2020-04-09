49c49805864ae7d00f3526114fd612895895e276
mrgumorist <mrgumorist@gmail.com>
2020-04-09 16:08:26 +0300

    idk

---
+++ app/src/main/java/com/example/begin/productview/click_listeners/OnEditListener.java
@@ -0,0 +1,7 @@
+package com.example.begin.productview.click_listeners;
+
+import com.example.begin.network.ProductEntry;
+
+public interface OnEditListener {
+    public void editItem(ProductEntry productEntry, int index);
+}
